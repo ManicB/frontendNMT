0c1330ee5debf04a16c6eb35d62508a88ac0293e
igor talic <igor talic>
2019-07-11 22:33:16 +0200

    Initial commit message

---
+++ src/components/DodajNovoMesto.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+      <div class="col-6 col-md-3">
+        <input type="text" class="form-control" placeholder="Mesto" v-model="mesto">
+      </div>
+      <div class="col-6 col-md-3">
+        <input type="text" class="form-control" placeholder="Broj stanovnika" v-model="broj_stanovnika">
+      </div>
+      <div class="col-6 col-md-3">
+        <input type="text" class="form-control" placeholder="PTT broj" v-model="ptt_broj">
+      </div>
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button" @click.prevent="newQuote">Dodaj mesto</button>
+      </div>
+    </div> 
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
+
+
